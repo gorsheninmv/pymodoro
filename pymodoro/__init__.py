ddb6958388f7b1eb5b0eca10f368aeb2dd796680
Misha Gorshenin <gorshenin.mv@gmail.com>
2021-01-06 20:37:42 +0500

    Run app through pymodoro command prefix

--- pymodoro/__init__.py
+++ pymodoro/__init__.py
@@ -1,2 +1,18 @@
-def main():
-    return 'Ok'
+import sys
+import tomatonotifier
+import tomatorunner
+
+
+def main() -> None:
+    app = sys.argv[1] if len(sys.argv) > 1 else ''
+
+    if app == 'runner':
+        tomatorunner.main()
+    elif app == 'notifier':
+        tomatonotifier.main()
+    else:
+        inner_main()
+
+
+def inner_main() -> None:
+    print('pymodoro!')
