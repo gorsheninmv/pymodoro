1cfa9d371dcfcfe7e4eb0ec7059e772ed6c5f669
Misha Gorshenin <gorshenin.mv@gmail.com>
2021-01-06 23:55:34 +0500

    Improve notifier UI

--- tomatonotifier/__init__.py
+++ tomatonotifier/__init__.py
@@ -1,14 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+import PyQt5.QtWidgets as widgets
+import PyQt5.QtCore as qtcore
 
 
 def main():
-    app = QApplication(sys.argv)
-    w = QWidget()
-    b = QPushButton(w)
-    b.setText("Show message!")
-    b.move(50, 50)
-    b.clicked.connect(lambda: app.exit(0))
-    w.setWindowTitle("Tomato Timer")
-    w.show()
+    app = widgets.QApplication(sys.argv)
+    win = widgets.QWidget()
+    win.setWindowTitle("Tomato Timer")
+    win.setFixedSize(200, 100)
+
+    label = widgets.QLabel()
+    label.setText('Hello from tomato timer')
+    label.setAlignment(qtcore.Qt.AlignCenter)
+
+    continue_btn = widgets.QPushButton()
+    continue_btn.setText("Work")
+    continue_btn.clicked.connect(lambda: app.exit(0))
+
+    relax_btn = widgets.QPushButton()
+    relax_btn.setText("Relax")
+    relax_btn.clicked.connect(lambda: app.exit(0))
+
+    hlayout = widgets.QHBoxLayout()
+    hlayout.setSpacing(5)
+    hlayout.addWidget(continue_btn)
+    hlayout.addWidget(relax_btn)
+
+    vlayout = widgets.QVBoxLayout()
+    vlayout.setSpacing(5)
+    vlayout.addWidget(label)
+    vlayout.addLayout(hlayout)
+
+    win.setLayout(vlayout)
+    win.show()
     sys.exit(app.exec_())
